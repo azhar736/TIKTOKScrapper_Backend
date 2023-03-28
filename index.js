fe4bc38f4c50a62c377bf412925122c320fea600
basher786 <mubsharalee@gmail.com>
2023-03-29 01:16:55 +0500

    Adding More events

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
-app.get("/", (req, res) => {
-  res.send("Welcome to Home Route");
-});
-const username = "malloryhecox1";
+// console.log("The User Before API hit", username);
 //Serever
 const server = app.listen(port, () => {
   console.log(`Server is running  on port ${port}`);
@@ -29,93 +26,68 @@ io = require("socket.io")(server, {
   },
 });
 
-// // Create a new connection.
-const tiktokConnection = new WebcastPushConnection(username);
-// Connect to the chat with our new object.
-tiktokConnection
-  .connect()
-  .then((state) => {
-    console.info(`Connected to ${state.roomId}'s live`);
-  })
-  .catch((err) => {
-    console.error("Failed to connect", err);
-  });
+app.post("/getusers", (req, res) => {
+  console.log(req.body.user);
+  let username = req.body.user;
+  // // Create a new connection.
+  const tiktokConnection = new WebcastPushConnection(username);
+  // Connect to the chat with our new object.
 
-// Listen for messages/Chat from the TikTok live stream.
-// tiktokConnection.on("chat", (data) => {
-//   console.log(`User nickname: ${data.nickname}, Commented: ${data.comment}`);
-//   /*
-//       Example of the log:
-//       User nickname: GreatPerson101, Commented: I love this stream!
-//     */
-// });
+  tiktokConnection
+    .connect()
+    .then((state) => {
+      console.info(`Connected to ${state.roomId}'s live`);
+    })
+    .catch((err) => {
+      console.error("Failed to connect", err);
+    });
 
-// Listen for likes from the TikTok live stream.
-// tiktokConnection.on('like', data => {
-//     console.log(`${data.uniqueId} sent ${data.likeCount} likes, total likes: ${data.totalLikeCount}`);
-// })
-
-// Listen for Gifts from the TikTok live stream.
-// tiktokConnection.on("gift", (data) => {
-//   if (data.giftType === 1 && !data.repeatEnd) {
-//     // Streak in progress => show only temporary
-//     console.log(
-//       `${data.uniqueId} is sending gift ${data.giftName} x${data.repeatCount}`
-//     );
-//   } else {
-//     // Streak ended or non-streakable gift => process the gift with final repeat_count
-//     console.log(
-//       `${data.uniqueId} has sent gift ${data.giftName} x${data.repeatCount}`
-//     );
-//   }
-// });
-
-// Listen for Gifts from the TikTok live stream.
-// tiktokConnection.on("share", (data) => {
-//   console.log(data.uniqueId, "shared the stream!");
-// });
-
-io.on("connection", (socket) => {
-  console.log("Socket is connected!!!", socket.id);
-  // Listen for messages/Chat from the TikTok live stream.
-  tiktokConnection.on("chat", (data) => {
-    console.log(`User nickname: ${data.nickname}, Commented: ${data.comment}`);
-    socket.emit("nickname", `${data.nickname}`);
-    socket.emit("comment", `${data.comment}`);
-  });
-
-  // Listen for likes from the TikTok live stream.
-  tiktokConnection.on("like", (data) => {
-    console.log(
-      `${data.uniqueId} sent ${data.likeCount} likes, total likes: ${data.totalLikeCount}`
-    );
-    socket.emit("user", `${data.uniqueId}`);
-    socket.emit("likes", `${data.likeCount}`);
-    socket.emit("totallikes", `${data.totalLikeCount}`);
-  });
-
-  // Listen for Gifts from the TikTok live stream.
-  tiktokConnection.on("gift", (data) => {
-    if (data.giftType === 1 && !data.repeatEnd) {
-      // Streak in progress => show only temporary
+  io.on("connection", (socket) => {
+    console.log("Socket is connected!!!", socket.id);
+    // Listen for messages/Chat from the TikTok live stream.
+    tiktokConnection.on("chat", (data) => {
       console.log(
-        `${data.uniqueId} is sending gift ${data.giftName} x${data.repeatCount}`
+        `User nickname: ${data.nickname}, Commented: ${data.comment}`
+      );
+      socket.emit("nickname", `${data.nickname}`);
+      socket.emit("comment", `${data.comment}`);
+    });
+
+    // Listen for likes from the TikTok live stream.
+    tiktokConnection.on("like", (data) => {
+      console.log(
+        `${data.uniqueId} sent ${data.likeCount} likes, total likes: ${data.totalLikeCount}`
       );
       socket.emit("user", `${data.uniqueId}`);
-      socket.emit("name", `${data.giftName}`);
-    } else {
-      // Streak ended or non-streakable gift => process the gift with final repeat_count
-      console.log(
-        `${data.uniqueId} has sent gift ${data.giftName} x${data.repeatCount}`
-      );
-      socket.emit("user", `${data.uniqueId}`);
-      socket.emit("name", `${data.giftName}`);
-    }
+      socket.emit("likes", `${data.likeCount}`);
+      socket.emit("totallikes", `${data.totalLikeCount}`);
+    });
+
+    // Listen for Gifts from the TikTok live stream.
+    tiktokConnection.on("gift", (data) => {
+      if (data.giftType === 1 && !data.repeatEnd) {
+        // Streak in progress => show only temporary
+        console.log(
+          `${data.uniqueId} is sending gift ${data.giftName} x${data.repeatCount}`
+        );
+        socket.emit("user", `${data.uniqueId}`);
+        socket.emit("giftname", `${data.giftName}`);
+      } else {
+        // Streak ended or non-streakable gift => process the gift with final repeat_count
+        console.log(
+          `${data.uniqueId} has sent gift ${data.giftName} x${data.repeatCount}`
+        );
+        socket.emit("user", `${data.uniqueId}`);
+        socket.emit("name", `${data.giftName}`);
+      }
+    });
+
+    // Listen for Gifts from the TikTok live stream.
+    // tiktokConnection.on("share", (data) => {
+    //   console.log(data.uniqueId, "shared the stream!");
+    // });
+    // socket.emit("user", `${data.uniqueId}`);
   });
 
-  // Listen for Gifts from the TikTok live stream.
-  // tiktokConnection.on("share", (data) => {
-  //   console.log(data.uniqueId, "shared the stream!");
-  // });
-  // socket.emit("user", `${data.uniqueId}`);
+  res.send({ success: true, message: "connection is established!" });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const { registerUser, loginUser } = require("../controller/userController");
 const router = require("express").Router();
 router.post("/register", registerUser);
-router.get("/login", loginUser);
+router.post("/login", loginUser);
 
 module.exports = router;
